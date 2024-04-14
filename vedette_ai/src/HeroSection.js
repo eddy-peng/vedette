071780f849e0717b24d9cfd7c8fdbe1e8065da24
sfoell <91638246+sfoell@users.noreply.github.com>
2024-04-14 13:27:52 -0700

    adjusts hero section, adds image folder

--- vedette_ai/src/HeroSection.js
+++ vedette_ai/src/HeroSection.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import PrimaryBtn from './Components/PrimaryBtn';
 
 export default function HeroSection() {
     return (
-        <div className='hero-section'>
+        <section className='hero-section'>
             <h1>Welcome to <span className='text-gradient'>Vedette 1.0</span></h1>
-            <p className='sub-title'>Automating report deduplication on Google’s Vulnerability Reward Program for Bug Reporters</p>
-        </div>
+            <div className='hero-subsection'>
+                <p className='sub-title'>Automating report deduplication on Google's Vulnerability <br></br> Reward Program for Bug Reporters</p>
+                <div className='button-container'>
+                    <PrimaryBtn />
+                    <a className='secondary-button'>LEARN MORE</a>
+                </div>
+            </div>
+            <img src='images/Problem.svg'></img>
+        </section>
     )
 }
