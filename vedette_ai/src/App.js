dc4c01eca7d3f6c5ca2963d55eeeb5398d97cbc3
Eddy Peng <eddyp2@uw.edu>
2024-04-18 18:14:30 -0700

    eddy has been humbled by routing.

--- vedette_ai/src/App.js
+++ vedette_ai/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import './App.css';
 import NavBar from './Components/Navbar';
 import Footer from './Components/Footer';
@@ -8,15 +9,21 @@ import MilestonePage from './MilestonePage';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
+  const { pathname } = useLocation();
+
+  // Scroll to the top whenever the location changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div>
       {/* Navbar */}
       <NavBar />
-      
       {/* Page routes */}
       <Routes>
         <Route index element={<SolutionPage />} />
-        <Route path='solution'element={<SolutionPage />} />
+        <Route path='solution' element={<SolutionPage />} />
         <Route path='team' element={<TeamPage />} />
         <Route path='milestones' element={<MilestonePage />} />
       </Routes>
