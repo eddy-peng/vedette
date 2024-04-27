4bea8b1e9b9d2d65f587dc596d958ee84ebafef4
Harold Pham <63668080+HaroldPham@users.noreply.github.com>
2024-04-26 21:28:18 -0700

    root connection

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { HashRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = document.getElementById('root')
 root.render(
   <Router>
     <App />
