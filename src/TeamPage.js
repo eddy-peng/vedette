fa0adc9b706230d8f86c42df698905994128796c
Eddy Peng <eddyp2@uw.edu>
2024-05-19 21:17:01 -0700

    team philosophy section

--- src/TeamPage.js
+++ src/TeamPage.js
@@ -3,11 +3,13 @@ import TeamIntroSection from './TeamSections/TeamIntroSection';
 import CampusVisitSection from './TeamSections/CampusVisitSection';
 import GuidingPrinciplesSection from './TeamSections/GuidingPrinciplesSection';
 import BonusTeamPhotos from './TeamSections/BonusTeamPhotos';
+import Roles from './TeamSections/Roles';
 
 export default function TeamPage() {
     return (
         <div>
             <TeamIntroSection />
+            <Roles />
             <GuidingPrinciplesSection />
             <CampusVisitSection />
             <BonusTeamPhotos />
