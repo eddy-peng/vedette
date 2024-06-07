f534d97431fb152ddfbd25fee622781352c6dee8
Eddy Peng <eddyp2@uw.edu>
2024-06-07 09:39:20 -0700

    final 2

--- src/MilestonePage.js
+++ src/MilestonePage.js
@@ -2,11 +2,15 @@ import React from 'react';
 import MilestoneCards from './MilestoneSections/MilestoneCards';
 import NotableMilestones from './MilestoneSections/NotableMilestones';
 import FutureActions from './MilestoneSections/FutureActions';
+import AwardWinning from './MilestoneSections/AwardWinning';
+import FeaturedCapstone from './MilestoneSections/FeaturedCapstone';
 
 export default function MilestonePage() {
     return (
         <div>
             <MilestoneCards />
+            <FeaturedCapstone />
+            <AwardWinning />
             <NotableMilestones />
             <FutureActions />
         </div>
