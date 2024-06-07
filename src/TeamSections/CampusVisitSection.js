154d678559caa622a9aaec99b59b406f6097251f
Eddy Peng <eddyp2@uw.edu>
2024-06-07 09:40:11 -0700

    final 5

--- src/TeamSections/CampusVisitSection.js
+++ src/TeamSections/CampusVisitSection.js
@@ -9,8 +9,9 @@ export default function CampusVisitSection() {
                 <h2>Google Campus Visits</h2>
                 <p className='heading-caption'>Throughout the creation of Vedette, several workshops at the Google campus in Kirkland, WA were conducted alongside the Android Security Team to learn, build, and connect.</p>
             </div>
-            <div className="img-container">
+            <div className="img-container woah-its-greg">
                 <img src={campusvisits} className='campus-visit-img' alt='Project team at Google campus'></img>
+                <p className='feature-desc'>After the first campus visit, the team took a moment to remember the fine day.</p>
             </div>
         </section>
     )
